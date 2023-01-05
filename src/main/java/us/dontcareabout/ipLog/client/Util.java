10d1de41604f5cd85d3b2ef55922dd7ab5ff88a6
MontyPan <monty.pan@dontcareabout.us>
2023-01-05 21:36:48 +0800

    增加：Util.dateFormat

---
+++ src/main/java/us/dontcareabout/ipLog/client/Util.java
@@ -0,0 +1,8 @@
+package us.dontcareabout.ipLog.client;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+public class Util {
+	public static final DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy/MM/dd HH:mm:ss");
+
+}
