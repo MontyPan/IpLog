81db34de9e7f8b756d5026793c9fd639de36c011
MontyPan <monty.pan@dontcareabout.us>
2023-01-08 01:41:35 +0800

    增加：minuteCeilDiff()

--- src/main/java/us/dontcareabout/ipLog/client/Util.java
+++ src/main/java/us/dontcareabout/ipLog/client/Util.java
@@ -1,8 +1,20 @@
 package us.dontcareabout.ipLog.client;
 
+import java.util.Date;
+
 import com.google.gwt.i18n.client.DateTimeFormat;
 
 public class Util {
 	public static final DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy/MM/dd HH:mm:ss");
 
+	/**
+	 * @return 兩個時間相差幾分鐘，無條件進位至整數
+	 */
+	public static int minuteCeilDiff(Date start, Date end) {
+		if (start.equals(end)) { return 1; }
+
+		return (int)Math.ceil(
+			1.0 * (end.getTime() - start.getTime()) / (60 * 1000)
+		);
+	}
 }
