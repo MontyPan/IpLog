4272ff7c3f9500a83503dc4b2e08c0ddbcb705cc
MontyPan <monty.pan@dontcareabout.us>
2023-01-08 22:32:27 +0800

    增加：asOverlapList()

--- src/main/java/us/dontcareabout/ipLog/client/vo/IpData.java
+++ src/main/java/us/dontcareabout/ipLog/client/vo/IpData.java
@@ -16,10 +16,13 @@ public class IpData {
 	public final String ip;
 	public final int validityHour;
 
+	/** 為了 {@link #asOverlapList()} 方便寫，所以存一下 */
+	private List<RawData> rawList = new ArrayList<>();
 	private List<NamePeriod> periodList = new ArrayList<>();
 	private Set<String> nameSet = new HashSet<>();
 	private Date start =  new Date();
 	private Date end = new Date(0);
+	private List<IpData> overlapList;
 
 	public IpData(String ip, int validityHour) {
 		this.ip = ip;
@@ -28,6 +31,9 @@ public class IpData {
 
 	public void add(RawData raw) {
 		if (!ip.equals(raw.ip)) { return; }	//習慣性預防 XD
+
+		overlapList = null;	//預防 asOverlapMode() 之後又 add（謎之聲：誰敢？）
+		rawList.add(raw);
 
 		//raw 一定會加進來，所以稱這個時候處理 start / end
 		if (start.after(raw.date)) { start = raw.date; }
@@ -75,6 +81,49 @@ public class IpData {
 	 */
 	public Date getEnd() {
 		return end;
+	}
+
+	/**
+	 * 會把有時間重疊的部份切出來成獨立的 {@link IpData}。
+	 * 會忽略「時間沒有重疊的資料」。
+	 */
+	public List<IpData> asOverlapList() {
+		if (overlapList != null) { return overlapList; }
+
+		//預防萬一，還是一律排序一下
+		rawList.sort((e1, e2) -> e1.date.compareTo(e2.date));
+
+		ArrayList<IpData> result = new ArrayList<>();
+		IpData tmp = new IpData(ip, validityHour);
+		tmp.add(rawList.get(0));
+
+		for (int i = 1; i < rawList.size(); i++) {
+			RawData raw = rawList.get(i);
+
+			if (inValidity(raw.date, tmp.getEnd(), validityHour)) {
+				tmp.add(raw);
+				continue;
+			}
+
+			//raw 的時間已經超過時限
+			if (tmp.periodList.size() != 1) {  //有 overlap
+				result.add(tmp);
+			}
+
+			tmp = new IpData(ip, validityHour);
+			tmp.add(raw);
+		}
+
+		//最後得再判斷一次有沒有 overlap
+		if (tmp.periodList.size() != 1) {
+			result.add(tmp);
+		}
+
+		return result;
+	}
+
+	public boolean hasOverlap() {
+		return asOverlapList().size() > 0;
 	}
 
 	/**
