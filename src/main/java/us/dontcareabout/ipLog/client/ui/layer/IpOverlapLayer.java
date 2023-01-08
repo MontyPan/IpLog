49bc441ef005753fd23d75d88af9a2ca5a3b9201
MontyPan <monty.pan@dontcareabout.us>
2023-01-08 22:34:18 +0800

    增加：IpOverlapLayer

---
+++ src/main/java/us/dontcareabout/ipLog/client/ui/layer/IpOverlapLayer.java
@@ -0,0 +1,30 @@
+package us.dontcareabout.ipLog.client.ui.layer;
+
+import us.dontcareabout.gxt.client.draw.LSprite;
+import us.dontcareabout.gxt.client.draw.layout.HorizontalLayoutLayer;
+import us.dontcareabout.ipLog.client.vo.IpData;
+
+public class IpOverlapLayer extends HorizontalLayoutLayer {
+	public IpOverlapLayer(IpData data) {
+		setGap(3);
+		data.asOverlapList().stream().forEach(slim -> {
+			IpDataLayer ipDL = new IpDataLayer(slim);
+			addChild(ipDL, ipDL.getViewSize());
+		});
+
+		adjustMember();
+	}
+
+	public double computeHeight() {
+		double result = 0;
+
+		for (LSprite sprite : getMembers()) {
+			if (sprite instanceof IpDataLayer) {
+				IpDataLayer ipDL = (IpDataLayer)sprite;
+				result = Math.max(result, ipDL.computeHeight());
+			}
+		}
+
+		return result;
+	}
+}
