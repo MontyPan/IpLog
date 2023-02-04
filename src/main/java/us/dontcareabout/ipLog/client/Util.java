e42b1f9e015dd7d926200c1fd4d099af75641ac1
MontyPan <monty.pan@dontcareabout.us>
2023-02-04 15:48:44 +0800

    增加：IpData map 的 toString()

--- src/main/java/us/dontcareabout/ipLog/client/Util.java
+++ src/main/java/us/dontcareabout/ipLog/client/Util.java
@@ -1,8 +1,13 @@
 package us.dontcareabout.ipLog.client;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.google.gwt.i18n.client.DateTimeFormat;
+
+import us.dontcareabout.ipLog.client.vo.IpData;
 
 public class Util {
 	public static final DateTimeFormat dateFormat = DateTimeFormat.getFormat("yyyy/MM/dd HH:mm:ss");
@@ -24,5 +29,54 @@ public class Util {
 		return (int)Math.ceil(
 			1.0 * (end.getTime() - start.getTime()) / (60 * 60 * 1000)
 		);
+	}
+
+
+	public static String toString(Map<String, IpData> ipMap) {
+		StringBuilder result = new StringBuilder();
+
+		for (String ip : ipMap.keySet()) {
+			IpData ipData = ipMap.get(ip);
+			if (ipData.getNameList().size() < 2) { continue; }
+			if (!ipData.hasOverlap()) { continue; }
+
+			result.append(toString(ip, ipData.asOverlapList()));
+			result.append("\n\n");
+		}
+
+		return result.toString();
+	}
+
+	private static String toString(String ip, List<IpData> overlapList) {
+		StringBuilder result = new StringBuilder("《" + ip + "》");
+		overlapList.forEach(ipd -> {
+			//時間區間
+			result.append("\n");
+			result.append(
+				Util.dateFormat.format(ipd.getStart()) + "～" +
+				Util.dateFormat.format(ipd.getEnd()) +
+				"（" + Util.hourCeilDiff(ipd.getStart(), ipd.getEnd()) + "hr）"
+			);
+
+			HashMap<String, Integer> countMap = new HashMap<>();
+
+			ipd.getPeriodList().forEach(np -> {
+				Integer count = countMap.get(np.name);
+
+				if (count == null) {
+					countMap.put(np.name, np.getCount());
+				} else {
+					countMap.put(np.name, np.getCount() + count);
+				}
+			});
+
+			//名稱 : 次數
+			countMap.keySet().forEach(name -> {
+				result.append("\n    " + name + " : " + countMap.get(name));
+			});
+			result.append("\n");
+		});
+
+		return result.toString();
 	}
 }
